ac11021d3ab1ed0b98cfa5edc4a0622e9c03c6ac
CRABDE <crabdeoff1.0@gmail.com>
2026-01-08 01:20:10 +0100

    Add Netlify URL to CORS

--- server.js
+++ server.js
@@ -16,9 +16,8 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://localhost:8000',
-  // Ajoutez votre domaine en production ici :
-  // 'https://votre-site.netlify.app',
-  // 'https://votre-site.vercel.app'
+  'https://crabde-site.netlify.app',
+
 ];
 
 const corsOptions = {
